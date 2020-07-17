e239fdedfff3e4054085ec393300782aa3efca61
halsheik <halsheik@umich.edu>
2020-07-16 20:22:42 -0400

    My Recipes Page CSS and OverLay Added

--- public/controls/navControl.js
+++ public/controls/navControl.js
@@ -1,11 +1,13 @@
 const dropMenu = function(){
     const dropDownMenuButton = document.querySelector('.dropDownMenuButton');
     const allLinks = document.querySelector('.allLinks');
+    const overlay = document.querySelector('.overlay');
 
     // Toggle dropDownMenu
     dropDownMenuButton.addEventListener('click', function(){
         allLinks.classList.toggle('openDropDownMenu');
         dropDownMenuButton.classList.toggle('moveButtonUp');
+        overlay.classList.toggle('addOverlay');
     });
 }
 
